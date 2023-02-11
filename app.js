f6668687eb538e5af838684d9d7cd560b712071a
rajkumar-1997 <124024525+rajkumar-1997@users.noreply.github.com>
2023-02-11 22:00:19 +0530

    done

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('hello world');
+let headertitle=document.getElementById('header-title');
+headertitle.style.border='solid 2px black';
+// console.log(headertitle);
+
+let title=document.getElementsByClassName('title');
+// console.log(title);
+title[0].style.fontWeight='bold';
+title[0].style.color='green';
