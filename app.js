6924b3e993ec307cbb91919d064f58f294d5f76e
rajkumar-1997 <124024525+rajkumar-1997@users.noreply.github.com>
2023-02-11 22:16:51 +0530

    getelementbyclass

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ let title=document.getElementsByClassName('title');
 // console.log(title);
 title[0].style.fontWeight='bold';
 title[0].style.color='green';
+
+let item=document.getElementsByClassName('list-group-item');
+
+item[0].style.fontWeight='bold';
+item[1].style.fontWeight='bold';
+item[2].style.fontWeight='bold';
+item[3].style.fontWeight='bold';
+item[2].style.backgroundColor='green';
